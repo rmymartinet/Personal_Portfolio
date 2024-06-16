544a386896bd09653ba049f46c839e8cbdfefb17
martinet <remymartinet.contact@gmail.com>
2024-06-16 18:53:12 +0200

    Update data file

--- app/data/data.js
+++ app/data/data.js
@@ -1,18 +1,36 @@
 const images = [
   {
-    image: "/images/city.jpg",
-    title: "title",
-    subtitle: "subtitle",
+    image: "/images/margritt/margritt.jpg",
+    img1: "/images/margritt/img1.png",
+    img2: "/images/margritt/img2.png",
+    img3: "/images/margritt/img3.png",
+    img4: "/images/margritt/img4.png",
+    img5: "/images/margritt/img5.png",
+    title: "Margritt",
+    subtitle:
+      "Margritt is a artist. She wanted a portfolio to showcase her work. She wanted to have a gallery to showcase her work.",
+    year: "2024",
+    role: "FullStack dev & Motion",
+    type: "Portfolio",
+    client: "Margritt",
   },
   {
-    image: "/images/car.jpg",
-    title: "title",
+    image: "/images/streamworld.jpg",
+    title: "Streamworld",
     subtitle: "subtitle",
+    year: "2024",
+    type: "Streaming",
+    role: "FullStack dev & Motion",
+    client: "Own project",
   },
   {
-    image: "/images/water.jpg",
-    title: "title",
+    image: "/images/netflix.jpg",
+    title: "Netflix Clone",
     subtitle: "subtitle",
+    year: "2023",
+    type: "Streaming",
+    role: "FullStack dev & Motion",
+    client: "Clone",
   },
 ];
 
