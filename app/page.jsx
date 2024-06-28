1bf83f5858af04d0f438daec6c472b87e3585b71
martinet <remymartinet.contact@gmail.com>
2024-06-28 21:29:25 +0200

    Refactored animations for better cleanliness, maintainability, and readability, Deleted ref not use

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@
 import WhiteDivBottom from "@/components/Home/WhiteDivBottom";
 import WhiteDivTop from "@/components/Home/WhiteDivTop";
 import { useHomeNavigationStore } from "@/store/useHomeNavigation";
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
@@ -12,11 +13,10 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function MyApp() {
   const containerRef = useRef(null);
-  const fixedContainerRef = useRef(null);
   const blinkingTimeRef = useRef(null);
-  const rightContentRef = useRef(null);
   const [isScrolling, setIsScrolling] = useState(false);
   const { isHomeClicked } = useHomeNavigationStore();
+  const homeContainerRef = useRef(null);
 
   /**
    * !TODO Ajouter le fais que l'on ne puisse pas scroller entre les animations
@@ -58,108 +58,103 @@ export default function MyApp() {
     };
   }, []);
 
-  useEffect(() => {
-    gsap.to(blinkingTimeRef.current, {
-      opacity: 0,
-      repeat: -1,
-      yoyo: true,
-      duration: 1,
-    });
-  }, []);
+  useGSAP(
+    () => {
+      gsap.to(blinkingTimeRef.current, {
+        opacity: 0,
+        repeat: -1,
+        yoyo: true,
+        duration: 1,
+      });
+    },
+    { scope: homeContainerRef.current }
+  );
 
   return (
-    <>
-      <main>
-        <div onWheel={handleWheel} ref={containerRef}>
+    <section ref={homeContainerRef}>
+      <div onWheel={handleWheel} ref={containerRef}>
+        <div className="fixed top-0 left-0 h-screen w-full">
           <div
-            ref={fixedContainerRef}
-            className="fixed top-0 left-0 h-screen w-full"
+            className="relative grid"
+            style={{
+              gridTemplateColumns: "1.5fr 2fr",
+            }}
           >
-            <div
-              className="relative grid"
-              style={{
-                gridTemplateColumns: "1.5fr 2fr",
-              }}
-            >
-              <div className="h-screen">
-                <Image
-                  width={700}
-                  height={700}
-                  className="object-cover h-full w-full"
-                  src="/images/r.jpg"
-                  alt=""
-                  objectFit="cover"
-                />
-              </div>
-              <div className="px-10 h-full flex flex-col gap-y-10 justify-center">
-                <div className="flex">
-                  <p className="text-md uppercase w-1/6 flex-0.5 opacity-40">
-                    [ About ]
-                  </p>
-                  <div className="flex flex-col gap-5 px-24 leading-5 flex-1">
-                    <p>
-                      Hey there! I&apos;m a Front-end developer, specializing in
-                      React. Always eager to learn and explore, I&apos;m a true
-                      adventurer, with professional experience in web design and
-                      idea creation through my start-up. What I love about this
-                      job is the constant evolution.
-                    </p>
-                    <p>
-                      My early journey in the tech world has allowed me to
-                      leverage my competitive spirit and passion for quality
-                      work. I always strive to give my best and push my limits.
-                      I enjoy tackling projects or problems that are above my
-                      current skill level because they push me out of my comfort
-                      zone and help me grow. I work on a variety of projects, so
-                      feel free to check out my GitHub.
-                    </p>
-                    <p>
-                      I have also started a YouTube channel and an Instagram
-                      account where I share various topics and projects about my
-                      coding world.
-                    </p>
-                  </div>
-                </div>
-                <div className="flex">
-                  <p className="w-1/6 text-md uppercase flex-0.5 opacity-40">
-                    [ Contact ]
-                  </p>
-                  <div
-                    ref={rightContentRef}
-                    className="flex px-24 gap-10 flex-1 justify-between"
-                  >
-                    <div className="flex gap-10">
-                      <p className="hover cursor-pointer">GitHub</p>
-                      <p className="hover cursor-pointer">LinkedIn</p>
-                      <p className="hover cursor-pointer">Youtube</p>
-                      <p className="hover cursor-pointer">Instagram</p>
-                    </div>
-                    <p className="hover cursor-pointer">Email</p>
-                  </div>
-                </div>
-                <div className="absolute py-5 bottom-0">
-                  <h1
-                    className="tracking-tight uppercase"
-                    style={{ fontSize: "4.3vw" }}
-                  >
-                    martinet rémy 2024 ®
-                  </h1>
-                  <div className=" px-2 flex gap-2 text-md opacity-40">
-                    <p>{`Local`}</p>
-                    <p className="ml-2">{time.hour}</p>
-                    <span ref={blinkingTimeRef}>:</span>
-                    <p>{time.minutes}</p>
-                  </div>
-                </div>
-              </div>
-              {isScrolling && <WhiteDivBottom />}
-              {isHomeClicked && <WhiteDivTop />}
+            <div className="h-screen">
+              <Image
+                width={700}
+                height={700}
+                className="object-cover h-full w-full"
+                src="/images/r.jpg"
+                alt=""
+                objectFit="cover"
+              />
             </div>
+            <div className="px-10 h-full flex flex-col gap-y-10 justify-center">
+              <div className="flex">
+                <p className="text-md uppercase w-1/6 flex-0.5 opacity-40">
+                  [ About ]
+                </p>
+                <div className="flex flex-col gap-5 px-24 leading-5 flex-1">
+                  <p>
+                    Hey there! I&apos;m a Front-end developer, specializing in
+                    React. Always eager to learn and explore, I&apos;m a true
+                    adventurer, with professional experience in web design and
+                    idea creation through my start-up. What I love about this
+                    job is the constant evolution.
+                  </p>
+                  <p>
+                    My early journey in the tech world has allowed me to
+                    leverage my competitive spirit and passion for quality work.
+                    I always strive to give my best and push my limits. I enjoy
+                    tackling projects or problems that are above my current
+                    skill level because they push me out of my comfort zone and
+                    help me grow. I work on a variety of projects, so feel free
+                    to check out my GitHub.
+                  </p>
+                  <p>
+                    I have also started a YouTube channel and an Instagram
+                    account where I share various topics and projects about my
+                    coding world.
+                  </p>
+                </div>
+              </div>
+              <div className="flex">
+                <p className="w-1/6 text-md uppercase flex-0.5 opacity-40">
+                  [ Contact ]
+                </p>
+                <div className="flex px-24 gap-10 flex-1 justify-between">
+                  <div className="flex gap-10">
+                    <p className="hover cursor-pointer">GitHub</p>
+                    <p className="hover cursor-pointer">LinkedIn</p>
+                    <p className="hover cursor-pointer">Youtube</p>
+                    <p className="hover cursor-pointer">Instagram</p>
+                  </div>
+                  <p className="hover cursor-pointer">Email</p>
+                </div>
+              </div>
+              <div className="absolute py-5 bottom-0">
+                <h1
+                  className="tracking-tight uppercase"
+                  style={{ fontSize: "4.3vw" }}
+                >
+                  martinet rémy 2024 ®
+                </h1>
+                <div className=" px-2 flex gap-2 text-md opacity-40">
+                  <p>{`Local`}</p>
+                  <p className="ml-2">{time.hour}</p>
+                  <span ref={blinkingTimeRef}>:</span>
+                  <p>{time.minutes}</p>
+                </div>
+              </div>
+            </div>
+            {isScrolling && <WhiteDivBottom />}
+            {isHomeClicked && <WhiteDivTop />}
           </div>
         </div>
-        <div className="h-screen"></div>
-        <div className="h-screen"></div>
-      </main>
-    </>
+      </div>
+      <div className="h-screen"></div>
+      <div className="h-screen"></div>
+    </section>
   );
 }
